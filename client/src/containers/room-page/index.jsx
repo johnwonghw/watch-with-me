cedbfa93daea468942deda190a33a6b6a4a13556
John Wong <johnwong@Johns-MacBook-Pro-2.local>
2020-08-15 19:46:36 -0700

    removed simple-peer logic on clientside. Add sync when seeking time in VideoPlayer

--- client/src/containers/room-page/index.jsx
+++ client/src/containers/room-page/index.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import _helper from 'utils/helper';
 import socketIOClient from "socket.io-client";
-import SimplePeer from 'simple-peer';
-import VieoPlayer from 'containers/video-player';
+import VideoPlayer from 'containers/video-player';
 import { connect } from 'react-redux';
 
 import { Link } from "react-router-dom";
@@ -17,102 +16,77 @@ class RoomPage extends Component {
     this.peer = null;
     this.state = {
       connections: 0,
-      isLeader: false,
       currVideo: '',
       videoValue: '',
-      playing: false,
-
+      playing: true,
       messageValue: '',
       messageList: [],
-      
+
 
     }
   }
 
   componentDidMount() {
-    console.log('welcome to the room: ', this.props.match.params.roomId)
-    // this.initSocket()
+    console.log('welcome to the roossms: ', this.props.match.params.roomId)
+    this.initSocket()
   }
 
-  // initSocket = () => {
-  //   let roomId = this.props.match.params.roomId;
-  //   if (process.env.NODE_ENV === 'production') {
-  //     this.socket = socketIOClient();
-  //   } else {
-  //     this.socket = socketIOClient('localhost:4300');
-  //   }
-  //   this.socket.on('connect', () => {
-  //     this.socket.emit('join-room', {
-  //       roomId,
-  //     })
+  initSocket = () => {
+    let roomId = this.props.match.params.roomId;
+    if (process.env.NODE_ENV === 'production') {
+      this.socket = socketIOClient();
+    } else {
+      this.socket = socketIOClient('localhost:4300');
+    }
 
-  //     this.socket.on('update-room-data', (data) => {
-  //       let {
-  //         roomClientCount
-  //       } = data;
-  //       this.setState({
-  //         connections: roomClientCount,
-  //         isLeader: this.state.isLeader || roomClientCount === 1 ? true : false
-  //       })
-  //     })
+    this.socket.on('connect', () => {
+      this.socket.emit('join-room', {
+        roomId,
+      })
+    })
 
+    this.socket.on('update-room-data', (data) => {
+      let {
+        roomClientCount
+      } = data;
+      this.setState({
+        connections: roomClientCount
+      })
+    })
 
+    this.socket.on('update-video', (data) => {
+      let { videoSrc } = data;
+      this.setState({
+        currVideo: videoSrc
+      })
+    })
 
-  //     this.socket.on('peer', (data) => {
-  //       let peerId = data.peerId;
-  //       this.peer = new SimplePeer({ initiator: data.initiator, trickle: false, });
+    this.socket.on('room-message', (data) => {
+      console.log({ data })
+      this.setState({
+        messageList: [...this.state.messageList, data],
+      })
+    })
 
-  //       this.socket.on('signal', (data) => {
-  //         if (data.peerId == peerId) {
-  //           console.log('happening')
-  //           this.peer.signal(data.signal);
-  //         }
-  //       })
+    this.socket.on('play-video', () => {
+      this.setState({ playing: true })
+    })
 
-  //       this.peer.on('signal', (data) => {
-  //         this.socket.emit('signal', {
-  //           signal: data,
-  //           peerId
-  //         })
-  //       })
+    this.socket.on('pause-video', () => {
+      this.setState({ playing: false })
+    })
 
-  //       this.peer.on('connect', () => {
-  //         console.log('Peer connection established');
-  //         this.peer.send(JSON.stringify({
-  //           type: "log",
-  //           value: "hi my peers"
-  //         }));
-  //       });
+    this.socket.on('seek-video', (data) => {
+      console.log(data.played)
+      // TODO should not be called for original seeker as they will be 'seeking' twice.
+      this.player.seekTo(data.played)
+    })
 
-  //       this.peer.on('data', (data) => {
-  //         console.log('rtc received data: ', data)
-  //         let dec = new TextDecoder("utf-8")
-  //         let parsedData = JSON.parse(dec.decode(data))
-  //         let { type, playing, message } = parsedData;
-  //         // let { playing } = data;
-  //         if (type === 'video-action') {
-  //           this.setState({ playing })
-  //         } else if (type === 'message') {
-  //           this.setState({ messageList: [...this.state.messageList, message] })
-  //         } else if (type === 'log') {
-  //           console.log('rtc log: ', parsedData.value)
-  //         }
-  //       })
+    this.socket.on('log', (data) => {
+      console.log('logging: ', data)
+    })
 
-  //     })
-
-  //     this.socket.on('update-video', (data) => {
-  //       let { videoSrc } = data;
-  //       this.setState({
-  //         currVideo: videoSrc
-  //       })
-  //     })
-
-  //     this.socket.on('log', (data) => {
-  //       console.log('logging: ', data)
-  //     })
-  //   })
-  // }
+  }
 
   changeVid = () => {
     let { videoValue } = this.state;
@@ -122,30 +96,18 @@ class RoomPage extends Component {
     })
   }
 
-  handleClickPlay = () => {
-    this.peer.send(JSON.stringify({
-      type: 'video-action',
-      playing: !this.state.playing
-    }))
-    this.setState({
-      playing: !this.state.playing
-    })
+  handleVideoAction = (playing) => {
+    this.socket.emit(playing ? 'play-video' : 'pause-video')
+    this.setState({ playing })
   }
 
   handleSendMessage = () => {
     if (this.state.messageValue) {
-      let messageBody = {
-        "username": "me",
-        "text": this.state.messageValue
-      }
+      this.socket.emit('room-message', {
+        username: "asdf",
+        text: this.state.messageValue
+      })
 
-      this.peer.send(JSON.stringify({
-        type: "message",
-        message: {
-          "username": "weirdo",
-          "text": this.state.messageValue
-        }
-      }))
       this.setState({
         messageList: [...this.state.messageList, {
           "username": "me",
@@ -153,39 +115,45 @@ class RoomPage extends Component {
         }],
         messageValue: ''
       })
+
     }
   }
 
+  playerRef = player => {
+    this.player = player
+  }
+
+  handleSeekTo = (frac) => {
+    this.socket.emit('seek-video', {
+      played: frac
+    });
+
+    this.player.seekTo(frac);
+  }
+
   render() {
-    console.log(this.state)
+    // console.log(this.state)
     return (
       <div className="front-page-container">
         <div>There are currently {this.state.connections} people in this room!</div>
-        <div>{this.state.isLeader ? 'I am the leader!' : 'I am just a follower'}</div>
-        <button onClick={() => {
-          this.socket.emit('dataa', 'bob the builda')
-        }}>send</button>
-        <button onClick={() => {
-          this.peer.send('hello can u hear me?')
-        }}>simplo</button>
-
-        {this.state.isLeader
-          ? <div>
-            <input
-              name="video-src"
-              onChange={(e) => {
-                this.setState({ videoValue: e.target.value })
-              }}
-              value={this.state.videoValue}
-            />
-            <button onClick={this.changeVid}>Change vid</button>
-          </div>
-          : ''}
 
         <div>
-          <VieoPlayer
+          <input
+            name="video-src"
+            onChange={(e) => {
+              this.setState({ videoValue: e.target.value })
+            }}
+            value={this.state.videoValue}
+          />
+          <button onClick={this.changeVid}>Change vid</button>
+        </div>
+
+        <div>
+          <VideoPlayer
+            handleSeekTo={this.handleSeekTo}
+            playerRef={this.playerRef}
             url={this.state.currVideo}
-            handleClickPlay={this.handleClickPlay}
+            handleVideoAction={this.handleVideoAction}
             playing={this.state.playing}
           />
         </div>
--- client/src/containers/video-player/index.jsx
+++ client/src/containers/video-player/index.jsx
@@ -10,29 +10,36 @@ class VideoPlayer extends Component {
 
     this.state = {
       isReady: false,
-      // playing: false,
       showControls: false,
+      playedSeconds: 0,
+      totalSeconds: 0,
     }
   }
 
-  handleOnReady = () => {
-    this.setState({ isReady: true })
+  getMinuteDisplay = (totalSeconds) => {
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = ("0" + (totalSeconds % 60)).slice(-2);
+    return `${minutes}:${seconds}`
   }
 
-  // handleOnPlay = () => {
-  //   this.setState({ status: 'playing' })
-  // }
+  handleOnReady = () => {
+    this.setState({ isReady: true, playedSeconds: 0, totalSeconds: 0 })
+  }
 
-  // handleOnPause = () => {
-  //   this.setState({ status: 'paused' })
-  // }
+  handleOnPlay = () => {
+    this.props.handleVideoAction(true);
+  }
+
+  handleOnPause = () => {
+    this.props.handleVideoAction(false);
+  }
 
   getPlayPauseBtn = () => {
     let { playing } = this.props;
     let click = () => {
-      this.props.handleClickPlay();
-      // this.setState({ playing: !playing }) 
+      this.props.handleVideoAction(!playing);
     }
+
     if (playing) {
       return (
         <FaPause color="white" onClick={click} />
@@ -42,23 +49,22 @@ class VideoPlayer extends Component {
     }
   }
 
-  handleMouseMove = () => {
-    console.log('asf')
-    // this.setState({ showControls: true })
-
-    // let fadeOutTimeout = setTimeout(() => {
-    //   this.setState({ showControls: false})
-    // }, 3000);
+  timeSeekerRef = timeSeeker => {
+    this.timeSeeker = timeSeeker
   }
 
   render() {
     let {
+      playerRef,
       url,
       playing,
+      handleSeekTo
     } = this.props;
 
     let {
-      showControls
+      showControls,
+      playedSeconds,
+      totalSeconds
     } = this.state;
 
     return (
@@ -69,7 +75,10 @@ class VideoPlayer extends Component {
       >
         <div className="player-wrapper">
           <ReactPlayer
-            url={'https://www.youtube.com/watch?v=yXrlhebkpIQ'}
+            // url={url}
+            volume={0}
+            url={'https://www.youtube.com/watch?v=khZK783T6_8'}
+            ref={playerRef}
             className="react-player"
             width='100%'
             height='100%'
@@ -77,13 +86,29 @@ class VideoPlayer extends Component {
             onReady={this.handleOnReady}
             onPlay={this.handleOnPlay}
             onPause={this.handleOnPause}
+            // onSeek={() => console.log('abc')}
             playing={playing}
+            progressInterval={250}
+            onDuration={(totalSeconds) => { this.setState({ totalSeconds: Math.ceil(totalSeconds) }) }}
+            onProgress={({ loaded, loadedSeconds, played, playedSeconds }) => {
+              this.setState({ playedSeconds: Math.ceil(playedSeconds) })
+            }}
           />
         </div>
         <div className={`controls-wrapper ${showControls ? '' : 'hide'}`}>
-          {this.state.status}
           <div className="play-pause-btn-wrapper">
             {this.getPlayPauseBtn()}
+          </div>
+          <div className="time-seeker-wrapper">
+            <div ref={this.timeSeekerRef} className="time-seeker-bar" onClick={(e) => {
+              const position = e.clientX - this.timeSeeker.offsetLeft;
+              const barWidth = this.timeSeeker.offsetWidth
+              const positionPercent = position / barWidth;
+              handleSeekTo(positionPercent)
+            }}>
+              <div className="time-seeker-bar-played" style={{ width: `${(playedSeconds / totalSeconds) * 100}%` }}></div>
+            </div>
+            <div className="time-seeker-time">{this.getMinuteDisplay(playedSeconds)} / {this.getMinuteDisplay(totalSeconds)}</div>
           </div>
         </div>
       </div>
